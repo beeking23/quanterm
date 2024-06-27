69d31a59f989d865f3f9f28f85419c0f9f0737b7
beeking23 <bee.king.23@gmail.com>
2024-06-27 12:11:11 +0100

    functional include was missing on pi

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <algorithm>
 #include <cctype>
 #include <cmath>
+#include <functional>
 
 #include <cairo.h>
 
